6e199528bb33cb4e400b2e329cd280594bb05de4
srikarganji <srikarganji2004@gmail.com>
2025-04-11 15:40:22 +0530

    1st

---
+++ Trie.java
@@ -0,0 +1,19 @@
+// class ImpleTrie{
+//     public static void main(String[] args) {
+//         System.out.println("Trie Implementation");
+//         Scanner sc=new Scanner(System.in);
+//         int t=sc.nextInt();
+//         while(t>0){
+
+
+//         }
+//     }
+// }
+// class Trie{
+//     char child[]=new char[26];
+//     int wc;
+//     boolean ended;
+
+    
+
+// }
